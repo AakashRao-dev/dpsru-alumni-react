38285f8596e4a53ed6dbc860830877812c12e645
Aakash <aakash.codes02@gmail.com>
2023-02-06 18:56:37 +0530

    added changes to the NotFoundPage

--- src/pages/notfoundpage/NotFoundPage.jsx
+++ src/pages/notfoundpage/NotFoundPage.jsx
@@ -4,16 +4,21 @@ import Button from '../../components/Button';
 
 const NotFoundPage = () => {
   return (
-    <div className="relative w-full min-h-screen">
-      <h1>Nothing to see here</h1>
-      <p>
-        Page you are trying to open does not exist. You may have mistyped the
-        address, or the page has been moved to another URL. If you think this is
-        an error contact support.
-      </p>
-      <Link to="/">
-        <Button ctaTitle="Take me back to Home Page" />
-      </Link>
+    <div className="relative w-full min-h-screen bg-black/90 overflow-hidden">
+      <h1 className="absolute -top-1/4 left-[17%] text-[504px] hidden md:block -z-50 text-black/50 font-black">
+        404
+      </h1>
+      <div className="flex flex-col justify-end min-h-screen mx-auto items-center max-w-xl text-center gap-8 pb-14 text-light-gray/80 z-50">
+        <h2 className="text-4xl font-bold">Nothing to see here</h2>
+        <p className="text-lg">
+          Page you are trying to open does not exist. You may have mistyped the
+          address, or the page has been moved to another URL. If you think this
+          is an error contact support.
+        </p>
+        <Link to="/">
+          <Button ctaTitle="Take me back to Home Page" paddingY="2" />
+        </Link>
+      </div>
     </div>
   );
 };
